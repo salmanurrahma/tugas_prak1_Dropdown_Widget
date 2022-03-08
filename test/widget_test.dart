2484672b7c2c9bee322d7b5ccad30b3bf675c553
salmanurrahma <99798368+salmanurrahma@users.noreply.github.com>
2022-03-08 16:58:34 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,8 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart'; 
-import 'package:jobsheet_2/main.dart' show MyApp;
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:tugas_prak1_suhu/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
